3bdddf80255ae5b2e1444aa65c6797b456ad9a3d
javad jafarian <100275868+javadjafarian@users.noreply.github.com>
2026-06-22 19:20:23 +0330

    Add CustomUser model with age and phone number fields; update settings for custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     age =  models.PositiveIntegerField(null=True, blank=True)
-    phone_nmuber =  models.CharField(max_length=50)
+    phone_nmuber =  models.CharField(max_length=11, blank=True)
