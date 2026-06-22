8e3cd0064254c9006788077264d59661173da253
javad jafarian <100275868+javadjafarian@users.noreply.github.com>
2026-06-22 19:42:57 +0330

    Fix phone number field name in CustomUser model and update admin configuration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+        fieldsets = (
+        ('Account Info', {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                    'phone_number',
+                    'age',
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+        add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "phone_number", "age", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     age =  models.PositiveIntegerField(null=True, blank=True)
-    phone_nmuber =  models.CharField(max_length=11, blank=True)
+    phone_number =  models.CharField(max_length=11, blank=True)
