856a8a5e93a83af4ccba35e2d6a57f14f40f1606
javad jafarian <100275868+javadjafarian@users.noreply.github.com>
2026-06-22 19:02:49 +0330

    create project

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    age =  models.PositiveIntegerField(null=True, blank=True)
+    phone_nmuber =  models.CharField(max_length=50)
